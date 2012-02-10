e6c283dcc5d2db1310ab712d7ae0766bdaa0e707
Art.py <lekcjephp@gmail.com>
2012-02-10 16:25:32 +0000

    koniec, projekt sokonczon

--- colored.py
+++ colored.py
@@ -1,9 +1,18 @@
 import os
 import sys
 
+COMMAND_NOT_FOUND_ERROR_CODE = 32512
+
+
 if os.getuid() == 0:
-	print ("jestes rootem")
+	print ("OK")
+	
 else:
 	print("sorry nie jestes rootem")	
 	sys.exit()#natychmiasowe wylacznei programu
 
+print("Za chwile bedziemy aktualizowac system..")
+if os.system('yaourt') == COMMAND_NOT_FOUND_ERROR_CODE:
+	os.system("pacman -Syu")
+else: 
+	os.system('yaourt -Syu')
